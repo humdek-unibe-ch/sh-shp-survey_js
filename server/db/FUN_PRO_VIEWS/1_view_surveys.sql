71c3f07e70bc432e709d9daa76be3b29c3c480dc
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-04-27 13:01:17 +0200

    if the survey has translation properly show the title

--- server/db/FUN_PRO_VIEWS/1_view_surveys.sql
+++ server/db/FUN_PRO_VIEWS/1_view_surveys.sql
@@ -1,5 +1,12 @@
 DROP VIEW IF EXISTS view_surveys;
 CREATE VIEW view_surveys
 AS
-SELECT id, survey_generated_id, created_at, updated_at, JSON_SET(config, '$.survey_generated_id', survey_generated_id) AS config, JSON_UNQUOTE(JSON_EXTRACT(config, '$.title')) AS survey_name
+SELECT id, survey_generated_id, created_at, updated_at, JSON_SET(config, '$.survey_generated_id', survey_generated_id) AS config, 
+JSON_UNQUOTE(
+    IF(
+      JSON_CONTAINS_PATH(config, 'one', '$.title.default'),
+      JSON_EXTRACT(config, '$.title.default'),
+      JSON_EXTRACT(config, '$.title')
+    )
+  ) AS survey_name
 FROM surveys;
