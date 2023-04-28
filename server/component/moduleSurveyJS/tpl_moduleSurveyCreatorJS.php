1ed5f291e7f0acde599f79a6d88ddb30aba8c428
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-04-28 13:39:30 +0200

    update surveyjs to v1.9.85 and properly pass the survey config to be loaded

--- server/component/moduleSurveyJS/tpl_moduleSurveyCreatorJS.php
+++ server/component/moduleSurveyJS/tpl_moduleSurveyCreatorJS.php
@@ -3,4 +3,4 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
-<div id="surveyJSCreator" data-config='<?php echo isset($fields['survey']['config']) ? $fields['survey']['config'] : ""; ?>'></div>
+<div id="surveyJSCreator" data-config='<?php echo isset($fields['survey']['config']) ? htmlspecialchars($fields['survey']['config'], ENT_QUOTES, 'UTF-8') : ""; ?>'></div>
